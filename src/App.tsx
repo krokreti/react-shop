68a566eae8cec3fa7300d1b5e804f0f530d0a98d
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-01-07 15:59:22 -0300

    Add Appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
 import './App.css';
-import CustomButton from './components/shared/CustomButton';
-import Input from './components/shared/Input';
-import { MdAndroid } from 'react-icons/md'
-import { useState } from 'react';
-import CustomLoadingButton from './components/shared/CustomLoadingButton';
+import Appbar from './components/layout/Appbar';
 
 function App() {
-  const [loading, setLoading] = useState(false);
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value)
-  }
-
-  const handleClick = () => {
-    console.log('ta funcionando')
-  }
-
-  const handleLoader = () => {
-    setLoading(!loading)
-  }
 
   return (
     <div className="App">
-      Teste
-      <Input id='id' type='text' label='Login' onChange={onChange} variant={"outlined"} />
-      <div>
-        <CustomButton text='Send' onClick={handleClick} startIcon={<MdAndroid/>}/>
-        <CustomLoadingButton loading={loading} text="Enviar" onClick={handleLoader}/>
-      </div>
+      <Appbar></Appbar>
     </div>
   );
 }
