9991f458894632670adca8a09183f7ca90d543d0
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-01-07 14:18:20 -0300

    Create default properties for values

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,29 +1,32 @@
 import { TextField } from "@mui/material";
 
+type VariantType = "outlined" | "filled" | "standard" | undefined
 
-const Input: React.FC<{ 
-    variant: "outlined", 
+type InputPropType = {
     id: string, 
     label: string, 
     type: string, 
+    variant: VariantType, 
     defaultValue?: string, 
     required?: boolean, 
     helperText?: string,
     value?: string,
     disabled?: boolean,
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
-}> = props => {
+}
+
+const Input: React.FC<InputPropType> = props => {
     return (
         <TextField 
             value={props.value}
             defaultValue={props.defaultValue}
-            variant={props.variant}
+            variant={props.variant ?? "outlined" }
             id={props.id}
             label={props.label}
             helperText={props.helperText}
-            required={props.required}
-            disabled={props.disabled}
-            type={props.type}
+            required={props.required ?? false}
+            disabled={props.disabled ?? false}
+            type={props.type ?? "text"}
             onChange={props.onChange}
         />
     )
