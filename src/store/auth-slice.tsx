2961caf84e903567cb78af450cb1e79731fba2c2
davi <davipinheiro1993@gmail.com>
2023-01-11 08:36:01 -0300

    create redux store

---
+++ src/store/auth-slice.tsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialAuthState = {
+    isAuthenticated: false,
+}
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+
+    }
+})
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
