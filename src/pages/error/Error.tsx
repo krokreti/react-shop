d201f9e836360d784fb876ad9012a58242b48ec3
davi <davipinheiro1993@gmail.com>
2023-01-25 11:30:06 -0300

    update error page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,7 +1,7 @@
 import { useRouteError, isRouteErrorResponse } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack';
-
+import Card from '../../components/shared/Card';
 
 const Error = () => {
     const error = useRouteError();
@@ -11,20 +11,34 @@ const Error = () => {
         return (
           <Box>
             <Stack
-              mt={4}
               direction="column"
               justifyContent="center"
               alignItems="center"
+              gap={4}
             >
-              <h1>Oops!</h1>
-              <h2>{error.status}</h2>
-              <p>{error.statusText}</p>
-              {error.data?.message && <p>{error.data.message}</p>}
+              <Card>
+                <h1>Oops!</h1>
+                <h2>An error ocurred!</h2>
+                <h2>{error.status}</h2>
+                <p>{error.statusText}</p>
+                {error.data?.message && <p>{error.data.message}</p>}
+              </Card>
             </Stack>
           </Box>
         );
       } else {
-        return <div>Oops</div>;
+        return (
+        <Card>
+          <Stack
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              gap={4}
+            >
+          <h1>Oops</h1>
+          <h3>An error ocurred!</h3>
+          </Stack>
+          </Card>);
       }
 }
 
